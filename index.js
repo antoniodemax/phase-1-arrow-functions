869bac6d78d8c1d928a8516198532e9b248f2e1f
Anthony <anthony.onyango@student.moringaschool.com>
2024-06-27 19:18:50 +0300

    update index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+// Function expression called divide
+const divide = function(divide) {
+    return 2000 / 100;
+    console.log(divide(2000/100));
+}
+//Arrow function called square
+const square = (num) => {
+    return num * num
+};
+console.log(square(2));
+
+//has an arrow function called add
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(4,3); //=>
+//add arrow function takes two parameters and adds them together
